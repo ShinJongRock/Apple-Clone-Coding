49119fa57cc234d33c96f46724e6171840821b2c
sjr0630@gmail.com <whdfkr0630@gmail.com>
2023-03-21 20:02:50 +0900

    스크롤애니메이션 구현2

---
+++ main.js
@@ -0,0 +1,80 @@
+(() => {
+    
+    let yOffset = 0; // 페이지 y옵셋 대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치보다 이전에 위치한 스크롤 세션들을 스크롤 높이값의 합
+    let currentScent = 0; // 현재 활성화된 씬 (세션)
+
+
+
+    const sceneInfo = [
+        // 0
+        {
+            type: 'sticky',
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅 함
+            scrollHeight:0,
+            objs:{
+                container: document.querySelector('#scroll-section-0')
+            }
+        },
+        // 1
+        {
+            type: 'normal',
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅 함
+            scrollHeight:0,
+            objs:{
+                container: document.querySelector('#scroll-section-1')
+            }
+        },
+        // 2
+        {
+            type: 'sticky',
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅 함
+            scrollHeight:0,
+            objs:{
+                container: document.querySelector('#scroll-section-2')
+            }
+        },
+        // 3
+        {
+            type: 'sticky',
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅 함
+            scrollHeight:0,
+            objs:{
+                container: document.querySelector('#scroll-section-3')
+            }
+        }
+
+        
+    ];
+
+
+    function setLayout() {
+        //각스크롤 높이 세팅
+        for(let i = 0; i< sceneInfo.length; i++){
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum*window.innerHeight;
+            sceneInfo[i].objs.container.style.height =`${sceneInfo[i].scrollHeight}px`;
+        }
+        console.log(sceneInfo)
+    }
+
+
+
+  
+    //콘솔 스크롤 위치값 출력
+    function scrollLoop(){
+        for (let i = 0; i < sceneInfo.length; i++){
+            prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
+        }
+        console.log(prevScrollHeight);
+    }
+
+    window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset
+        scrollLoop();
+    });
+
+    setLayout();
+
+
+})();
